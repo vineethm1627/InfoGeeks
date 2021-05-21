2c4e6f167b94f8b3b9017c1286b9c7bbaa981c41
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-05-21 19:26:48 +0530

    files modified

--- Backend/config.py
+++ Backend/config.py
@@ -1,5 +1,5 @@
 # keys and tokens from the Twitter Dev Console
-consumer_key = '6NonPbVdbs3KucwdX73HhZL8F'
-consumer_secret = 'oEwXsCoRImeTDuCaBwY54g9klGYVE322WoWP7ymsKbL00JkryL'
-access_token = '1182953255430774785-sGbIEfn41b08SN6oaFLxE04OfAMWyI'
-access_token_secret = 'AeHL936yNMALQxbu5DBrOFaBMxedccHyokEC2wMvkIoIt'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
